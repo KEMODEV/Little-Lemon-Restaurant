858f8918fcdf9c15a8eba70c6c1da8aa15319001
KEMODEV <tristanc1015@gmail.com>
2024-06-08 00:30:46 -0400

    Halfway finished with the Home Page
    Home page is finished up to the highlights/specials section. The "order a delivery" link is missing its svg because it shows a URL even ONLY when I use the correct path

--- little-lemon-restaurant/src/components/Header.js
+++ little-lemon-restaurant/src/components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import '../styles/Header.css'
+import logo from '../images/Logo .svg'
 
 const Header = () => {
   return (
     <header>
-        <img src='./images/Logo.svg' alt='Holding plate of food.' />
+        <a href='/'><img src={logo} alt='Little Lemon logo.' /></a>
         <nav>
-            <ul>
-              <li><a href='/'>Home</a></li>
-              <li><a href='/about'>About</a></li>
-              <li><a href='/menu'>Menu</a></li>
-              <li><a href='/reservations'>Reservations</a></li>
-              <li><a href='/order-online'>Order Online</a></li>
-              <li><a href='/login'>Login</a></li>
-            </ul>
+          <ul>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/about'>About</a></li>
+            <li><a href='/menu'>Menu</a></li>
+            <li><a href='/reservations'>Reservations</a></li>
+            <li><a href='/order-online'>Order Online</a></li>
+            <li><a href='/login'>Login</a></li>
+          </ul>
         </nav>
     </header>
   )
--- little-lemon-restaurant/src/components/Main.js
+++ little-lemon-restaurant/src/components/Main.js
@@ -1,9 +1,81 @@
 import React from 'react'
+import '../styles/Main.css'
+import heroImg from '../images/restauranfood.jpg'
+import greekSalad from '../images/greek salad.jpg'
+import bruchetta from '../images/bruchetta.jpg'
+import lemonDessert from '../images/lemon dessert.jpg'
+import deliveryIcon from '../images/delivery-scooter-icon.svg'
 
 const Main = () => {
   return (
     <main>
-        Main
+      <article id='heroSectionBg'>
+        <div className='center-section' id='heroSection'>
+          <div>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <button id='reserveTableBtn'>Reserve a Table</button>
+          </div>
+          <div>
+            <img
+              src={heroImg}
+              alt='Holding plate of Mediterranean food.'
+              id='heroImg'
+            />
+          </div>
+        </div>
+      </article>
+
+      <article className='center-section' id='highlightsSection'>
+        <section id='specialHeader'>
+          <h2>This week's special!</h2>
+          <button id='onlineMenuBtn'>Online Menu</button>
+        </section>
+        <section id='specialsCardsContainer'>
+          <div className='specials-cards'>
+            <div className='specials-img-container'>
+              <img src={greekSalad} alt='A greek salad.' />
+            </div>
+            <div className='specials-card-header'>
+              <h4>Greek Salad</h4>
+              <p>$12.99</p>
+            </div>
+            <div className='specials-card-body'>
+              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+              <aside><a>Order a delivery</a></aside>
+            </div>
+          </div>
+
+          <div className='specials-cards'>
+            <div className='specials-img-container'>
+              <img src={bruchetta} alt='A bruchetta.' />
+            </div>
+            <div className='specials-card-header'>
+              <h4>Bruchetta</h4>
+              <p>$5.99</p>
+            </div>
+            <div className='specials-card-body'>
+              <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+              <aside><a>Order a delivery</a></aside>
+            </div>
+          </div>
+
+          <div className='specials-cards'>
+            <div className='specials-img-container'>
+              <img src={lemonDessert} alt='A lemon cake dessert.' />
+            </div>
+            <div className='specials-card-header'>
+              <h4>Lemon Dessert</h4>
+              <p>$5.00</p>
+            </div>
+            <div className='specials-card-body'>
+              <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+              <aside><a>Order a delivery</a></aside>
+            </div>
+          </div>
+        </section>
+      </article>
     </main>
   )
 }
